65af9370bbdd16de36159023bf73a442df28a79b
yezlee <heemong9@gmail.com>
2020-09-10 17:16:09 +0900

    함수  .NULL 관련함수 (NVL2, NULLIF, COALESCE)
    조건절
     . CASE
     . DECODE
    
    함수
     . 그룹함수-개념
    
    실습
     .fn4~5
     . cond1~3

--- 20200909.sql
+++ 20200909.sql
@@ -53,7 +53,7 @@ comm 컬럼이 NULL일때 0으로 변경하여 sal 컬럼과 합계를 구한다
 SELECT empno, ename, sal, comm,
        sal + NVL(comm, 0) nvl_sum,
        sal + NVL2(comm, comm, 0) nvl2_sum,
-       NVL2(comm, sal+comm, sal) nvl2_sum2,
+       NVL2(comm, sal+comm, sal) nvl2_sum2,  --  comm이 null이 아니면 sal+comm, 그렇지 않으면 sal
        NULLIF(sal, sal) nullif,
        NULLIF(sal, 5000) nullif_sal,
        sal + COALESCE(comm, 0) coalesce_sum,
